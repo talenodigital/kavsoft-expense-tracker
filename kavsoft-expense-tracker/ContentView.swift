095e1260803a59ab966fc9005dc521ab8f351c4c
worldsdata <jose@worlds.org>
2023-09-12 16:05:21 +0800

    Initial Commit

---
+++ kavsoft-expense-tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  kavsoft-expense-tracker
+//
+//  Created by Joey Taleno on 9/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kavsoft-expense-tracker/kavsoft_expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  kavsoft_expense_trackerApp.swift
+//  kavsoft-expense-tracker
+//
+//  Created by Joey Taleno on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct kavsoft_expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
